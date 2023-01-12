037449beb141903ce25cb9a4d5e88326f799220d
Rafael Ciriello <ciriello.dev@gmail.com>
2023-01-12 10:43:27 +0000

    update

--- 003.4_exercise_pizza_order_practice.py
+++ 003.4_exercise_pizza_order_practice.py
@@ -1,34 +1,27 @@
 # Pizza Order Practice
-
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").upper()
+add_pepperoni = input("Do you want pepperoni? Y or N ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N ").upper()
 
 bill = 0
 
 if size == "S":
-    bill = 15
-    if add_pepperoni == "Y":
-        bill += 2
-    if extra_cheese == "Y":
-        bill += 1
-
+    bill += 15
 elif size == "M":
-    bill = 20
-    if add_pepperoni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
-
+    bill += 20
 elif size == "L":
-    bill = 25
-    if add_pepperoni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
-
+    bill += 25
 else:
     print("The selected pizza size does not exist!")
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
 
 print(f"Your final bill is: ${bill}.")
--- 003.6_exercise_love_calculator.py
+++ 003.6_exercise_love_calculator.py
@@ -1,24 +1,19 @@
 # Love Calculator
-
 print("Welcome to the Love Calculator!")
-name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
+name1 = input("What is your name? \n").lower()
+name2 = input("What is their name? \n").lower()
 
 combined_string = name1 + name2
-lower_case_string = combined_string.lower()
 
-t = lower_case_string.count("t")
-r = lower_case_string.count("r")
-u = lower_case_string.count("u")
-e = lower_case_string.count("e")
-
+t = combined_string.count("t")
+r = combined_string.count("r")
+u = combined_string.count("u")
+e = combined_string.count("e")
 true = t + r + u + e
 
-l = lower_case_string.count("l")
-o = lower_case_string.count("o")
-v = lower_case_string.count("v")
-e = lower_case_string.count("e")
-
+l = combined_string.count("l")
+o = combined_string.count("o")
+v = combined_string.count("v")
 love = l + o + v + e
 
 love_score = int(str(true) + str(love))
