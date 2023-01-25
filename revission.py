1d1397b442286c453fa02cea7b4cc62840c99691
Rafael Ciriello <ciriello.dev@gmail.com>
2023-01-25 16:30:33 +0000

    Create revission.py

---
+++ revission.py
@@ -0,0 +1,27 @@
+# from turtle import Turtle, Screen
+# jack = Turtle()
+#
+# print(jack)
+# jack.shape("turtle")
+# jack.color("cyan")
+# jack.forward(100)
+#
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+#
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
